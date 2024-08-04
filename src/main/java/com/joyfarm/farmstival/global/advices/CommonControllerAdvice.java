afad017a8cfad4c64056329aaa2ab77ae9c963e7
hyeon12 <seojh912_@naver.com>
2024-08-04 23:30:13 +0900

    토큰 발급과 인증

--- src/main/java/com/joyfarm/farmstival/global/advices/CommonControllerAdvice.java
+++ src/main/java/com/joyfarm/farmstival/global/advices/CommonControllerAdvice.java
@@ -1,7 +1,9 @@
 package com.joyfarm.farmstival.global.advices;
 
+import com.joyfarm.farmstival.global.Utils;
 import com.joyfarm.farmstival.global.exceptions.CommonException;
 import com.joyfarm.farmstival.global.rests.JSONData;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -11,7 +13,10 @@ import java.util.List;
 import java.util.Map;
 
 @RestControllerAdvice("com.joyfarm.farmstival")
+@RequiredArgsConstructor
 public class CommonControllerAdvice {
+
+    private final Utils utils;
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<JSONData> errorHandler(Exception e) {
@@ -21,6 +26,9 @@ public class CommonControllerAdvice {
         HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR; // 500
         if (e instanceof CommonException commonException) {
             status = commonException.getStatus();
+
+            // 에러 코드인 경우는 메세지 조회
+            if(commonException.isErrorCode()) message = utils.getMessage(e.getMessage());
 
             Map<String, List<String>> errorMessages = commonException.getErrorMessages();
             if (errorMessages != null) message = errorMessages;
--- src/main/java/com/joyfarm/farmstival/member/jwt/TokenProvider.java
+++ src/main/java/com/joyfarm/farmstival/member/jwt/TokenProvider.java
@@ -1,7 +1,9 @@
 package com.joyfarm.farmstival.member.jwt;
 
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import com.joyfarm.farmstival.global.exceptions.UnAuthorizedException;
+import com.joyfarm.farmstival.member.MemberInfo;
+import com.joyfarm.farmstival.member.services.MemberInfoService;
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import lombok.RequiredArgsConstructor;
@@ -22,6 +24,7 @@ public class TokenProvider {
 
     private final JwtProperties properties;
     private final AuthenticationManagerBuilder authenticationManagerBuilder;
+    private final MemberInfoService infoService;
 
     /**
      * JTW 토큰 생성
@@ -39,8 +42,6 @@ public class TokenProvider {
             Date validity = new Date(now + properties.getValidSeconds() * 1000);
             //현재 발급받은 시간에서 한 시간동안 유효
 
-            System.out.println("여기?" + properties.getValidSeconds());
-
             return Jwts.builder()
                     .setSubject(authentication.getName()) // 사용자 email(인증 완료)
                     .signWith(getKey(), SignatureAlgorithm.HS512) // 데이터 위변조 방지
@@ -50,14 +51,59 @@ public class TokenProvider {
         return null;
     }
 
-    // 토큰 정보로 인증한 회원 정보 가져오기
+    /**
+     * 토큰으로 회원 인증 객체 조회
+     * @param token
+     * @return
+     */
     public Authentication getAuthentication(String token){
-        return null;
+        //토큰 검증
+        validateToken(token);
+
+        Claims claims = Jwts.parser()
+                .setSigningKey(getKey())
+                .build()
+                .parseClaimsJws(token)
+                .getPayload();
+
+        String email = claims.getSubject();
+
+        MemberInfo memberInfo = (MemberInfo)infoService.loadUserByUsername(email);
+
+        return new UsernamePasswordAuthenticationToken(memberInfo, token, memberInfo.getAuthorities());
     }
 
-    // 토큰 검증 (유효한지, 만료되지 않았는지)
+    /**
+     * 토큰 유효성 검사
+     * @param token
+     */
     public void validateToken(String token){
+        String errorCode = null;
+        try{
+            Jwts.parser().setSigningKey(getKey()).build().parseSignedClaims(token).getPayload();
+        } catch(io.jsonwebtoken.security.SecurityException | MalformedJwtException e){
+            // 변조된 JWT 토큰
+            errorCode = "Malformed.jwt";
+            e.printStackTrace();
 
+        } catch(ExpiredJwtException e){
+            // 유효시간이 만료된 JWT 토큰
+            errorCode = "Expired.jwt";
+            e.printStackTrace();
+
+        } catch(UnsupportedJwtException e) {
+            // 지원되지 않는 형식의 JWT 토큰
+            errorCode = "Unsupported.jwt";
+            e.printStackTrace();
+
+        } catch(Exception e){
+            errorCode = "Error.jwt";
+            e.printStackTrace();
+        }
+
+        if(errorCode != null){
+            throw new UnAuthorizedException(errorCode);
+        }
     }
 
     private Key getKey(){
