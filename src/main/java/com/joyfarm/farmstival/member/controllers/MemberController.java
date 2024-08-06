4e672b5313691cbf5453418b8f593bc02603dd0a
heyejiyang <132132524+heyejiyang@users.noreply.github.com>
2024-08-06 22:58:33 +0900

    Update MemberController.java

--- src/main/java/com/joyfarm/farmstival/member/controllers/MemberController.java
+++ src/main/java/com/joyfarm/farmstival/member/controllers/MemberController.java
@@ -21,6 +21,7 @@ import org.springframework.web.bind.annotation.*;
 public class MemberController {
 
     private final JoinValidator joinValidator;
+    private final MemberSaveService saveService;
     private final Utils utils;
     private final TokenProvider tokenProvider;
 
@@ -34,6 +35,8 @@ public class MemberController {
         if (errors.hasErrors()){
             throw new BadRequestException(utils.getErrorMessages(errors));
         }
+
+        saveService.save(form);
 
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
