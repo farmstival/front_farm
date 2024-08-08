f76f6968ae88b70dee1992e0c8fd671e92282427
hyeon12 <seojh912_@naver.com>
2024-08-08 14:54:19 +0900

    공공 데이터 처리 ING...

---
+++ src/test/java/com/joyfarm/farmstival/farmfarm/DataTest.java
@@ -0,0 +1,29 @@
+package com.joyfarm.farmstival.farmfarm;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.io.File;
+import java.util.List;
+import java.util.Map;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public class DataTest {
+
+    @Autowired
+    private ObjectMapper om;
+
+    @Test
+    void test1() throws Exception{
+        File file = new File("D:/data/data1.json");
+        //List<Map<String, String>> items = om.readValue(file, new TypeReference<>() {});
+        //System.out.println(items);
+
+        List<Map<String, String>> tmp = om.readValue(file, new TypeReference<>() {});
+    }
+}
