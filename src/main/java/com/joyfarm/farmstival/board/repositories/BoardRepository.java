c1f7e0148e4c26c4879bbc5f305964260ec4f176
soeunl <soeun318@gmail.com>
2024-08-16 17:51:52 +0900

    boardController 작업중

--- src/main/java/com/joyfarm/farmstival/board/repositories/BoardRepository.java
+++
@@ -1,8 +0,0 @@
-package com.joyfarm.farmstival.board.repositories;
-
-import com.joyfarm.farmstival.board.entities.Board;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.querydsl.QuerydslPredicateExecutor;
-
-public interface BoardRepository extends JpaRepository<Board, String>, QuerydslPredicateExecutor<Board> {
-}
