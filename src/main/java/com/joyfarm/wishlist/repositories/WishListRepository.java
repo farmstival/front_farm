5558da9ba9fa5cbd7ef6296d3bfb8804bfb21128
soeunl <soeun318@gmail.com>
2024-08-28 10:23:54 +0900

    중복 파일 제거

--- src/main/java/com/joyfarm/wishlist/repositories/WishListRepository.java
+++
@@ -1,9 +0,0 @@
-package com.joyfarm.wishlist.repositories;
-
-import com.joyfarm.wishlist.entities.WishList;
-import com.joyfarm.wishlist.entities.WishListId;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.querydsl.QuerydslPredicateExecutor;
-
-public interface WishListRepository extends JpaRepository<WishList, WishListId>, QuerydslPredicateExecutor<WishList> {
-}
