8426958ae9762b1d8dfe2776a207abd357175885
kth232 <kth7537@naver.com>
2024-08-29 16:06:43 +0900

    json 태그 추가(회원가입)

--- src/main/java/com/joyfarm/farmstival/member/controllers/RequestJoin.java
+++ src/main/java/com/joyfarm/farmstival/member/controllers/RequestJoin.java
@@ -1,11 +1,16 @@
 package com.joyfarm.farmstival.member.controllers;
 
-import jakarta.validation.constraints.*;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.validation.constraints.AssertTrue;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 import java.util.UUID;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true) //Java 객체로 변환할 때 JSON에 포함된 속성 중에서 Java 객체에 없는 속성이 있더라도 무시함
 public class RequestJoin {
 
     private String gid = UUID.randomUUID().toString();
