fbf4fffddc20b192975bc3fe3d603c1e8ee2d81b
heyejiyang <xoxo99312@naver.com>
2024-08-29 17:13:53 +0900

    로그인한 사용자만 본인 게시글 수정 삭제 가능하도록 수정

--- src/main/java/com/joyfarm/farmstival/member/controllers/RequestJoin.java
+++ src/main/java/com/joyfarm/farmstival/member/controllers/RequestJoin.java
@@ -1,16 +1,22 @@
 package com.joyfarm.farmstival.member.controllers;
 
-import jakarta.validation.constraints.*;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.validation.constraints.AssertTrue;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 import java.util.UUID;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class RequestJoin {
 
     private String gid = UUID.randomUUID().toString();
 
-    @NotBlank @Email
+    @NotBlank
+    @Email
     private String email;
 
     @NotBlank @Size(min=8)
